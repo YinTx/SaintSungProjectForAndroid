0ee111404689f315c65b7e532515777cf7227d89
YinTx <1185313860@qq.com>
2017-10-31 16:19:59 +0800

    Fragment优化
    修改了Fragment的切换方式，切换由原来的每次切换替换成利用hide和show切换Fragment

--- SaintSungPmc/app/src/main/java/com/saintsung/saintsungpmc/MainActivity.java
+++ SaintSungPmc/app/src/main/java/com/saintsung/saintsungpmc/MainActivity.java
@@ -1,5 +1,7 @@
 package com.saintsung.saintsungpmc;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.os.Bundle;
@@ -17,6 +19,10 @@ import com.saintsung.saintsungpmc.fragment.MainPersonalFragment;
 public class MainActivity extends BaseActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
     private BottomNavigationView bottomNavigationView;
     private long exitTime = 0;//2次回退计时器
+    private MainHomeFragment mainHomeFragment = new MainHomeFragment();
+    private MainControlFragment mainControlFragment = new MainControlFragment();
+    private MainMapFragment mainMapFragment = new MainMapFragment();
+    private MainPersonalFragment mainPersonalFragment = new MainPersonalFragment();
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
@@ -38,40 +44,87 @@ public class MainActivity extends BaseActivity implements BottomNavigationView.O
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+        setSelect(0);
     }
 
+    /*
+     * 切换显示内容的fragment
+     * */
+    private void setSelect(int i) {
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();//创建一个事务
+        hideFragment(transaction);//我们先把所有的Fragment隐藏了，然后下面再开始处理具体要显示的Fragment
+        switch (i) {
+            case 0:
+                if (mainHomeFragment == null) {
+                    mainHomeFragment = new MainHomeFragment();
+                /*
+                 * 将Fragment添加到活动中，public abstract FragmentTransaction add (int containerViewId, Fragment fragment)
+                *containerViewId即为Optional identifier of the container this fragment is to be placed in. If 0, it will not be placed in a container.
+                 * */
+                    transaction.add(R.id.main_fragment, mainHomeFragment);//将微信聊天界面的Fragment添加到Activity中
+                } else {
+                    transaction.show(mainHomeFragment);
+                }
+                break;
+            case 1:
+                if (mainControlFragment == null) {
+                    mainControlFragment = new MainControlFragment();
+                    transaction.add(R.id.main_fragment, mainControlFragment);
+                } else {
+                    transaction.show(mainControlFragment);
+                }
+
+                break;
+            case 2:
+                if (mainMapFragment == null) {
+                    mainMapFragment = new MainMapFragment();
+                    transaction.add(R.id.main_fragment, mainMapFragment);
+                } else {
+                    transaction.show(mainMapFragment);
+                }
+
+                break;
+            case 3:
+                if (mainPersonalFragment == null) {
+                    mainPersonalFragment = new MainPersonalFragment();
+                    transaction.add(R.id.main_fragment, mainPersonalFragment);
+                } else {
+                    transaction.show(mainPersonalFragment);
+                }
+                break;
+
+            default:
+                break;
+        }
+        transaction.commit();//提交事务
+    }
+
+    /*
+     * 隐藏所有的Fragment
+     * */
+    private void hideFragment(FragmentTransaction transaction) {
+        if (mainHomeFragment != null) {
+            transaction.hide(mainHomeFragment);
+        }
+        if (mainControlFragment != null) {
+            transaction.hide(mainControlFragment);
+        }
+        if (mainMapFragment != null) {
+            transaction.hide(mainMapFragment);
+        }
+        if (mainPersonalFragment != null) {
+            transaction.hide(mainPersonalFragment);
+        }
+
+    }
+
+
+    //-----------------------------------------------------------------------------------------------------------------------------------------
     //初始化控件
     private void initView() {
         bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navi_view);
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
-        //打开程序初始化界面
-        getHomeFragment();
-    }
-
-    private void getHomeFragment() {
-        MainHomeFragment mainHomeFragment = new MainHomeFragment();
-        getFragmentManager().beginTransaction().replace(R.id.main_fragment, mainHomeFragment).commit();
-        getFragmentManager().beginTransaction().show(mainHomeFragment);
-    }
-
-    private void getControlFragment() {
-        MainControlFragment mainHomeFragment = new MainControlFragment();
-        getFragmentManager().beginTransaction().replace(R.id.main_fragment, mainHomeFragment).commit();
-        getFragmentManager().beginTransaction().show(mainHomeFragment);
-    }
-
-    private void getMapFragment() {
-        MainMapFragment mainHomeFragment = new MainMapFragment();
-        getFragmentManager().beginTransaction().replace(R.id.main_fragment, mainHomeFragment).commit();
-        getFragmentManager().beginTransaction().show(mainHomeFragment);
-
-
-    }
-
-    private void getPersonalFragment() {
-        MainPersonalFragment mainHomeFragment = new MainPersonalFragment();
-        getFragmentManager().beginTransaction().replace(R.id.main_fragment, mainHomeFragment).commit();
-        getFragmentManager().beginTransaction().show(mainHomeFragment);
 
     }
 
@@ -80,23 +133,24 @@ public class MainActivity extends BaseActivity implements BottomNavigationView.O
         switch (item.getItemId()) {
             //主页Home页面的Fragment
             case R.id.menu_main_home:
-                getHomeFragment();
+                setSelect(0);
                 break;
             //设备空的页面的Fragment
             case R.id.menu_main_control:
-                getControlFragment();
+                setSelect(1);
                 break;
             //地图页面的Fragment
             case R.id.menu_main_map:
-                getMapFragment();
+                setSelect(2);
                 break;
             //个人中心页面的Fragment
             case R.id.menu_main_personal:
-                getPersonalFragment();
+                setSelect(3);
                 break;
             default:
                 break;
         }
+
         return true;
     }
 }
