05e0f9019c08d5f68f06d1779c98505dbd2fe75e
YinTx <1185313860@qq.com>
2017-11-16 17:03:07 +0800

    s

--- SaintSungPmc/app/src/main/java/com/saintsung/saintsungpmc/configure/BaseActivity.java
+++ SaintSungPmc/app/src/main/java/com/saintsung/saintsungpmc/configure/BaseActivity.java
@@ -1,9 +1,13 @@
 package com.saintsung.saintsungpmc.configure;
 
+import android.graphics.PixelFormat;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.TelephonyManager;
+import android.util.Log;
 
 import com.saintsung.saintsungpmc.R;
 import com.saintsung.saintsungpmc.map.CheckPermissionsActivity;
+import com.saintsung.saintsungpmc.tools.DataProcess;
 import com.saintsung.saintsungpmc.tools.ToastUtil;
 
 /**
@@ -11,10 +15,17 @@ import com.saintsung.saintsungpmc.tools.ToastUtil;
  */
 //CheckPermissionsActivity   AppCompatActivity
 public class BaseActivity extends CheckPermissionsActivity {
+    public BaseApplication baseApplication;
     @Override
     protected void onStart() {
+        Log.e("BaseActivityTAG","onStart");
+        baseApplication= (BaseApplication) getApplication();
+        TelephonyManager tm = (TelephonyManager) this.getSystemService(TELEPHONY_SERVICE);
+        baseApplication.setIEME(DataProcess.ComplementZeor(tm.getDeviceId(),18));
         super.onStart();
         // 沉浸式状态栏
         ToastUtil.setColor(this, getResources().getColor(R.color.colorAccent));
+        //防止Fragment切换高德地图闪屏问题
+        getWindow().setFormat(PixelFormat.TRANSLUCENT);
     }
 }
