4324388e03798f775f2375de4b8f9f6f3b87509a
YinTx <1185313860@qq.com>
2018-04-23 10:28:14 +0800

    1
    1

--- SaintSungPmc/app/src/main/java/com/saintsung/saintsungpmc/fragment/MainHomeFragment.java
+++ SaintSungPmc/app/src/main/java/com/saintsung/saintsungpmc/fragment/MainHomeFragment.java
@@ -1,20 +1,21 @@
 package com.saintsung.saintsungpmc.fragment;
 
+import android.view.View;
+
 import com.saintsung.common.app.Fragment;
 import com.saintsung.saintsungpmc.R;
+
+import butterknife.BindView;
 
 /**
  * Created by XLzY on 2017/7/28.
  */
 
 public class MainHomeFragment extends Fragment {
-<<<<<<< HEAD
+
     @BindView(R.id.appbar)
     View mLayAppbar;
-=======
 
-
->>>>>>> parent of d995cf3... 蓝牙接口调试
     @Override
     protected int getContentLayoutId() {
         return R.layout.fragment_home;
