6d5e5f630df8aa8fe238e3421307785d551daaac
YinTx <1185313860@qq.com>
2018-04-26 22:03:12 +0800

    1
    1

--- SaintSungPmc/app/src/main/java/com/saintsung/saintsungpmc/configure/Constant.java
+++ SaintSungPmc/app/src/main/java/com/saintsung/saintsungpmc/configure/Constant.java
@@ -26,4 +26,5 @@ public class Constant {
     public static String LoginService = "http://192.169.3.39/modifysaintsung/api/";
     public static final String serviceTail="/api/";
     public static final String addressHttp="http://210.22.164.146:";
+    public static final String addressHttps="";
 }
